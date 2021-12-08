e8d35463b24d3fdf0274f4ed7d39a6522259fffe
Sarah Raguin <sarahraguin@gmail.com>
2021-12-08 10:26:02 -0500

    Working on step 3 - adding product in a loop

--- front/js/functions.js
+++ front/js/functions.js
@@ -2,26 +2,38 @@
 
 function getProducts() {
     fetch("http://localhost:3000/api/products")
-            .then (function(response){
-            if (response.ok){
-                return response.json();
-            }
+        .then (function(response){
+        if (response.ok){
+            return response.json();
+        }
         })
         .then (function(data){
-            for(let infoProduct of data){
-                document.write(infoProduct.imageUrl - infoProduct.name - infoProduct.description);
-            }
+            return addElement();
         })
         .catch (function(err){
             console.log("Erreur : " + err)
         });  
 }
 
-
-/* var mycars = [{name:'Susita'}, {name:'BMW'}];
-
-for (var car of mycars) 
-{
-  document.write(car.name + "<br />");
+// va chercher les éléments un par un dans l'API pour chaque produit
+function getElements(){
+    for(let product of data){
+    document.getElementById("_id");
+    document.getElementById("imageUrl");
+    document.getElementById("altText")
+    document.getElementById("name");
+    document.getElementById("description");
+//Va chercher les élements dans le html et non dans le json ? A revoir !!!
 }
- */
+}
+
+// cré un nouvel élément et ajouter des éléments dedans
+function addElement () {
+  let newDiv = document.createElement("div");
+  let newContent = document.createTextNode(getElements());
+  newDiv.appendChild(newContent);
+
+// ajoute le nouvel élément créé et son contenu dans le DOM
+  let currentDiv = document.getElementById('items');
+  document.body.insertBefore(newDiv, currentDiv);
+}
