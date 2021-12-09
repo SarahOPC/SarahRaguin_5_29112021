f393e46c4ebe8be25b29079c33e90d9f34a98914
Sarah Raguin <sarahraguin@gmail.com>
2021-12-09 10:00:36 -0500

    Working on for loop

--- front/js/functions.js
+++ front/js/functions.js
@@ -18,7 +18,11 @@ function getProducts() {
 // va chercher les éléments un par un dans l'API pour chaque produit
 function getElements(){
     for(let i = 0; i<data.length; i ++){
+// variable stockant les éléments de chaque produit à chaque tour de boucle
         let product = data[i];
+// variable permettant de stocker les éléments Json de chaque produit dans un objet js
+        let products = JSON.parse(product);
+// retourne les éléments de chaque produit dans le html
         addElement();
 }
 }
@@ -26,10 +30,13 @@ function getElements(){
 // cré un nouvel élément et ajouter des éléments dedans
 function addElement () {
   let newDiv = document.createElement("div");
-  let newContent = document.createTextNode(product._id, product.name, product.imageUrl, product.altText, product.description);
+  let newContent = document.createTextNode(products._id, products.name, products.imageUrl, products.altText, products.description);
   newDiv.appendChild(newContent);
 
 // ajoute le nouvel élément créé et son contenu dans le DOM
   let currentDiv = document.getElementById('items');
   document.body.insertBefore(newDiv, currentDiv);
 }
+
+// JSON.parse(nomdelavariablejsonaparserpourtransformerdujsonenobjetjs)
+// JSON.stringify(transformeunobjetjsennotationjson)
