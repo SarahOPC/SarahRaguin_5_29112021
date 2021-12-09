e87159d8f4be49a0bcbede15085c77f1f0fb1983
Sarah Raguin <sarahraguin@gmail.com>
2021-12-09 08:30:03 -0500

    Still working on for loop for the products

--- front/js/functions.js
+++ front/js/functions.js
@@ -17,20 +17,16 @@ function getProducts() {
 
 // va chercher les éléments un par un dans l'API pour chaque produit
 function getElements(){
-    for(let product of data){
-    document.getElementById("_id");
-    document.getElementById("imageUrl");
-    document.getElementById("altText")
-    document.getElementById("name");
-    document.getElementById("description");
-//Va chercher les élements dans le html et non dans le json ? A revoir !!!
+    for(let i = 0; i<data.length; i ++){
+        let product = data[i];
+        addElement();
 }
 }
 
 // cré un nouvel élément et ajouter des éléments dedans
 function addElement () {
   let newDiv = document.createElement("div");
-  let newContent = document.createTextNode(getElements());
+  let newContent = document.createTextNode(product._id, product.name, product.imageUrl, product.altText, product.description);
   newDiv.appendChild(newContent);
 
 // ajoute le nouvel élément créé et son contenu dans le DOM
