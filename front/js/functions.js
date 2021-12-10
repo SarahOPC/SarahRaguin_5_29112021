7cd4f6daf171e891c006140419ac15ef85712605
Sarah Raguin <sarahraguin@gmail.com>
2021-12-10 10:30:57 -0500

    Still working on the loop

--- front/js/functions.js
+++ front/js/functions.js
@@ -17,9 +17,13 @@ function getProducts() {
 
 // va chercher les éléments un par un dans l'API pour chaque produit
 function getElements(data){
-    console.log(data [0].name);
     let products;
     for(let i = 0; i<data.length; i ++){
+        console.log(data[i]._id);
+        console.log(data[i].name);
+        console.log(data[i].imageUrl);
+        console.log(data[i].altTxt);
+        console.log(data[i].description);
 // variable stockant les éléments de chaque produit à chaque tour de boucle
         addElement(product);
     }
@@ -29,18 +33,29 @@ function getElements(data){
 // crée un nouvel élément et ajouter des éléments dedans
 function addElement (product) {
     let myLink = document.createElement("a");
-    myLink.href = product._id
+    myLink.href = product._id;
 
-  let newDiv = document.createElement("div");
-    newDiv.appendChild( myLink )
+    let myName = document.createElement("h3");
+    myName.h3 = product.name;
 
-  let newContent = document.createTextNode(product._id, product.name, product.imageUrl, product.altText, product.description);
-  newDiv.appendChild(newContent);
+    let myImage = document.createElement("img");
+    myImage.img = product.imageUrl;
+
+    let myAltTxt = document.createElement("alt");
+    myAltTxt.alt = product.altTxt;
+
+    let myDescription = document.createElement("p");
+    myDescription.p = product.description;
+
+
+    let newDiv = document.createElement("div");
+    newDiv.appendChild(myLink);
+    newDiv.appendChild(myName);
+    newDiv.appendChild(myImage);
+    newDiv.appendChild(myAltTxt);
+    newDiv.appendChild(myDescription);
 
 // ajoute le nouvel élément créé et son contenu dans le DOM
   let currentDiv = document.getElementById('items');
   document.body.insertBefore(newDiv, currentDiv);
 }
-
-// JSON.parse(nomdelavariablejsonaparserpourtransformerdujsonenobjetjs)
-// JSON.stringify(transformeunobjetjsennotationjson)
