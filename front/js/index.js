f91b40f6d0e74f031bd94a96b29fa5e132b092b6
Sarah Raguin <sarahraguin@gmail.com>
2021-12-21 09:41:35 -0500

    Working on page product to insert title, price, ...

--- front/js/index.js
+++ front/js/index.js
@@ -1,2 +1,57 @@
-var productInformations = getProducts();
-/*createElement et appendChild sont déjà utilisés dans la boucle dans le fichier functions.js*/
+//-------------------------------------------------------------------//
+//--------------------- Functions for Index Page---------------------//
+//-------------------------------------------------------------------//
+
+//fonction pour communiquer avec l'API et récupérer les données.
+
+async function getProducts() {
+    return fetch("http://localhost:3000/api/products")
+    .then (function(response){
+        if (response.ok){
+            return response.json();
+        }
+    })
+    .then (function(data){
+        return addDataToDom(data);
+    })
+    .catch (function(err){
+        console.log("Erreur : " + err)
+    });  
+}
+
+// va chercher les éléments un par un dans l'API pour chaque produit
+// crée un nouvel élément et ajoute des éléments dedans
+
+function addDataToDom(data) {
+    for(let products of data){
+        
+        let myLink = document.createElement("a");
+        let myName = document.createElement("h3");
+        let myImage = document.createElement("img");
+        let myAltTxt = document.createElement("alt");
+        let myDescription = document.createElement("p");
+        let newDiv = document.createElement("article");
+        
+        myLink.href = `product.html?id=${products._id}`;
+        myName.textContent = products.name;
+        myImage.src = products.imageUrl;
+        myImage.alt = products.altTxt;
+        myDescription.textContent = products.description;
+        
+        myLink.appendChild(newDiv);
+        newDiv.appendChild(myName);
+        newDiv.appendChild(myImage);
+        newDiv.appendChild(myAltTxt);
+        newDiv.appendChild(myDescription);
+        
+        // ajoute le nouvel élément créé et son contenu dans le DOM
+        document.getElementById("items").appendChild(myLink);
+    }
+}
+
+//-------------------------------------------------------------------//
+//--------------------- Functions for Index Page---------------------//
+//-------------------------------------------------------------------//
+
+let productInformations = getProducts();
+/*createElement et appendChild sont déjà utilisés dans la boucle*/
--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,45 @@
-// Pour écouter l'événement onclick
 
-//document.getElementById("a").addEventListener(onclick, function(){
-    // Pour récupérer l'id dans l'URL
-    let productUrlId = window.location.search;
-        
-    // Pour extraire l'id avec URLSearchParams
-    let extractId = new URLSearchParams(productUrlId);
+// Pour récupérer l'id dans l'URL
+let productUrlId = window.location.search;
+
+// Pour extraire l'id avec URLSearchParams
+let extractId = new URLSearchParams(productUrlId);
+
+let finalExtractId = extractId.get("id");
+
+//---------------------------------------------------------------------//
+//--------------------- Functions for Product Page---------------------//
+//---------------------------------------------------------------------//
+
+let idUrl = `http://localhost:3000/api/products/${finalExtractId}`;
+async function getInformationOfProductById(id){
+    return fetch(idUrl)
+    .then (function(response){
+        if (response.ok){
+            return response.json();
+        }
+    })
+    .then (function(data){
+        return addInfoOfProductToDom(data);
+    })
+    .catch (function(err){
+        console.log("Erreur : " + err)
+    });  
+}
+
+function addInfoOfProductToDom(data){
+    // pour chaque élément de mon json spécifié au bon id
+    // si id ==>
+        document.getElementById(/*" id de mon html "*/).textContent(/*" contenu de mon json "*/);
+        // sinon
+        document.getElementsByClassName(/*" nom de la classe "*/).textContent(/*" contenu de mon json "*/)
+    }
     
-    let finalExtractId = extractId.get("_id");
-    alert(finalExtractId);
-    //return finalExtractId;
-//})
-
+    //---------------------------------------------------------------------//
+//--------------------- Functions for Product Page---------------------//
+//---------------------------------------------------------------------//
 
 // Réinjecter l'id dans le code pour trouver les bonnes informations reliées au produit ciblé dans l'url
 //faire un appel get de l'api avec id en paramètre
 
+let returnInformationsOfProduct = getInformationOfProductById(finalExtractId);
