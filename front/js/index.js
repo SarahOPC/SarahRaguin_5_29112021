2ae3e1372e387021f4e26852e22b9d2e4ecc9925
Sarah Raguin <sarahraguin@gmail.com>
2022-02-25 15:56:18 -0500

    Code refactoring

--- front/js/index.js
+++ front/js/index.js
@@ -20,30 +20,56 @@ async function getProducts() {
 
 function addProductsDataToDom(data) {
     for(let products of data){
-        
-        let myLink = document.createElement("a");
-        let myName = document.createElement("h3");
-        let myImage = document.createElement("img");
-        let myAltTxt = document.createElement("alt");
-        let myDescription = document.createElement("p");
-        let newDiv = document.createElement("article");
-        
-        myLink.href = `product.html?id=${products._id}`;
-        myName.textContent = products.name;
-        myImage.src = products.imageUrl;
-        myImage.alt = products.altTxt;
-        myDescription.textContent = products.description;
-        
-        myLink.appendChild(newDiv);
-        newDiv.appendChild(myName);
-        newDiv.appendChild(myImage);
-        newDiv.appendChild(myAltTxt);
-        newDiv.appendChild(myDescription);
-        
-        // ajoute le nouvel élément créé et son contenu dans le DOM
-        document.getElementById("items").appendChild(myLink);
+                
+        creationOfTags();
+        insertion();
+        addingElements(products);
     }
 }
 
 getProducts();
-/*createElement et appendChild sont déjà utilisés dans la boucle*/
+
+function creationOfTags(){
+    let myLink = document.createElement("a");
+    let myName = document.createElement("h3");
+    let myImage = document.createElement("img");
+    let myAltTxt = document.createElement("alt");
+    let myDescription = document.createElement("p");
+    let newDiv = document.createElement("article");
+    
+    return {myLink, myName, myImage, myAltTxt, myDescription, newDiv};
+    // fonction retourne un objet contenant les variables voulues(écriture abrégée de l'objet))
+    // Since the names of the properties are the same as the variables, you can shorten it
+}
+
+function insertion(){
+    let elements = creationOfTags();
+    let myLink =  elements.myLink;
+    let newDiv = elements.newDiv;
+    myLink.appendChild(newDiv);
+    let myName = elements.myName;
+    newDiv.appendChild(myName);
+    let myImage = elements.myImage;
+    newDiv.appendChild(myImage);
+    let myAltTxt = elements.myImage;
+    newDiv.appendChild(myAltTxt);
+    let myDescription = elements.myDescription;
+    newDiv.appendChild(myDescription);
+
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+        let items = document.getElementById("items").appendChild(myLink);
+        return items;
+}
+
+function addingElements(products){
+    let elements = creationOfTags();
+    let myLink = elements.myLink;
+    myLink.href = `product.html?id=${products._id}`;
+    let myName = elements.myName;
+    myName.textContent = products.name;
+    let myImage = elements.myImage;
+    myImage.src = products.imageUrl;
+    myImage.alt = products.altTxt;
+    let myDescription = elements.myDescription;
+    myDescription.textContent = products.description;
+}
