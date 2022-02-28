a60500086e62c90e4fe0ed61c7285970f2f5e0f5
Sarah Raguin <sarahraguin@gmail.com>
2022-02-28 12:04:01 -0500

    Confirmation.js refactoring

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,14 +1,14 @@
 function getOrderId(){
     // Pour récupérer l'id dans l'URL
-let orderUrlId = window.location.search;
+    let orderUrlId = window.location.search;
 
-// Pour extraire l'id avec URLSearchParams
-let extractOrderId = new URLSearchParams(orderUrlId);
+    // Pour extraire l'id avec URLSearchParams
+    let extractOrderId = new URLSearchParams(orderUrlId);
 
-let finalOrderId = extractOrderId.get("orderId");
+    let finalOrderId = extractOrderId.get("orderId");
 
-let orderId = document.getElementById("orderId");
-orderId.textContent = finalOrderId;
+    let orderId = document.getElementById("orderId");
+    orderId.textContent = finalOrderId;
 }
 
 getOrderId();
