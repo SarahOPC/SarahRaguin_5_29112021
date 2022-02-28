197b8fd3c13bbcbd0979a881088660ca8236bb60
Sarah Raguin <sarahraguin@gmail.com>
2022-02-28 11:45:37 -0500

    Index.js refactoring

--- front/js/index.js
+++ front/js/index.js
@@ -19,57 +19,33 @@ async function getProducts() {
 // crée un nouvel élément et ajoute des éléments dedans
 
 function addProductsDataToDom(data) {
-    for(let products of data){
-                
-        creationOfTags();
-        insertion();
-        addingElements(products);
+    for(let product of data){
+        let productCreated = createProductDiv(product);
+        document.getElementById("items").appendChild(productCreated);
     }
 }
 
-getProducts();
-
-function creationOfTags(){
+function createProductDiv(product) {
     let myLink = document.createElement("a");
     let myName = document.createElement("h3");
     let myImage = document.createElement("img");
     let myAltTxt = document.createElement("alt");
     let myDescription = document.createElement("p");
     let newDiv = document.createElement("article");
-    
-    return {myLink, myName, myImage, myAltTxt, myDescription, newDiv};
-    // fonction retourne un objet contenant les variables voulues(écriture abrégée de l'objet))
-    // Since the names of the properties are the same as the variables, you can shorten it
-}
 
-function insertion(){
-    let elements = creationOfTags();
-    let myLink =  elements.myLink;
-    let newDiv = elements.newDiv;
+    myLink.href = `product.html?id=${product._id}`;
+    myName.textContent = product.name;
+    myImage.src = product.imageUrl;
+    myImage.alt = product.altTxt;
+    myDescription.textContent = product.description;
+    
     myLink.appendChild(newDiv);
-    let myName = elements.myName;
     newDiv.appendChild(myName);
-    let myImage = elements.myImage;
     newDiv.appendChild(myImage);
-    let myAltTxt = elements.myImage;
     newDiv.appendChild(myAltTxt);
-    let myDescription = elements.myDescription;
     newDiv.appendChild(myDescription);
 
-    // ajoute le nouvel élément créé et son contenu dans le DOM
-        let items = document.getElementById("items").appendChild(myLink);
-        return items;
+    return myLink;
 }
 
-function addingElements(products){
-    let elements = creationOfTags();
-    let myLink = elements.myLink;
-    myLink.href = `product.html?id=${products._id}`;
-    let myName = elements.myName;
-    myName.textContent = products.name;
-    let myImage = elements.myImage;
-    myImage.src = products.imageUrl;
-    myImage.alt = products.altTxt;
-    let myDescription = elements.myDescription;
-    myDescription.textContent = products.description;
-}
+getProducts();
